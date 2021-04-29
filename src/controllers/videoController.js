09b0584619758f5e760590586770d5bb4a58b3eb
unryeong <leyng7@gmail.com>
2021-04-30 00:21:12 +0900

    Edit Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -9,7 +9,10 @@ export const getEdit = async (req, res) => {
     const {id} = req.params;
     const video = await Video.findById(id);
 
-    return res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
+    if (!video) {
+        return res.render("404", {pageTitle: "Video not found."});
+    }
+    return res.render("edit", {pageTitle: `Edit: ${video.title}`, video});
 };
 
 export const postEdit = (req, res) => {
@@ -50,7 +53,10 @@ export const watch = async (req, res) => {
     const {id} = req.params;
     const video = await Video.findById(id);
 
-    return res.render("watch", { pageTitle: video.title, video });
+    if (!video) {
+        return res.render("404", {pageTitle: "Video not found."});
+    }
+    return res.render("watch", {pageTitle: video.title, video});
 };
 
 export const home = async (req, res) => {
