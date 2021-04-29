bd0794d340cbe79e8a627347fa043c4786f2f33e
unryeong <leyng7@gmail.com>
2021-04-29 22:51:34 +0900

    Edit Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,43 +1,48 @@
 let videos = [
-  {
-    id: 1,
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-  },
-  {
-    id: 2,
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-  },
-  {
-    id: 3,
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-  },
+    {
+        id: 1,
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+    },
+    {
+        id: 2,
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+    },
+    {
+        id: 3,
+        title: "Third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+    },
 ];
 
 export const deleteVideo = (req, res) => {
     res.send("Delete Video")
 };
 
-export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
+export const getEdit = (req, res) => {
+    const {id} = req.params;
+    const video = videos[id - 1];
+    return res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
+};
+
+export const postEdit = (req, res) => res.render("edit", {pageTitle: "Edit"});
 
 export const search = (req, res) => res.send("Search Video");
 
 export const see = (req, res) => {
-
-    const {id} =  req.params;
+    const {id} = req.params;
     const video = videos[id - 1];
-    return res.render("watch", {pageTitle: `Watch ${video.title}`});
+    return res.render("watch", {pageTitle: `Watching ${video.title}`, video});
 };
 
 export const trending = (req, res) => {
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {deleteVideo, edit, see, upload} from "../controllers/videoController";
+import {deleteVideo, getEdit, postEdit, see, upload} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", see);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/edit", getEdit);
+videoRouter.post("/:id(\\d+)/edit", postEdit);
 
 export default videoRouter;
