8f89a6e80a4f90e19e33d45e9a5d17eed52602aa
unryeong <leyng7@gmail.com>
2021-05-16 23:33:18 +0900

    5.23 Middlewares part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -41,9 +41,7 @@ export const postEdit = async (req, res) => {
         await Video.findByIdAndUpdate(id, {
             title,
             description,
-            hashtags: hashtags
-                .split(",")
-                .map(word => word.startsWith("#") ? word : "#" + word)
+            hashtags
         });
 
         return res.redirect(`/videos/${id}`);
@@ -62,9 +60,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title,
             description,
-            hashtags: hashtags
-                .split(",")
-                .map(word => word.startsWith("#") ? word : "#" + word)
+            hashtags
         });
         return res.redirect("/");
     } catch (error) {
