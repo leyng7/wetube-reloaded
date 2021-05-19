14df4f265969f02f34bfb3c205858f3322d126b9
unryeong <leyng7@gmail.com>
2021-05-19 21:51:04 +0900

    6.7 Sessions and Cookies part One

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import session from "express-session";
 import morgan from "morgan";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -12,6 +13,21 @@ app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(
+    session({
+        secret: "Hello!",
+        resave: true,
+        saveUninitialized: true
+    })
+)
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
