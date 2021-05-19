d851871485a2ceec586ece827b03a08d21f58e0d
unryeong <leyng7@gmail.com>
2021-05-19 21:10:33 +0900

    6.5, 6 Login part

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const edit = (req, res) => res.send("Edit User");
 
@@ -21,17 +22,46 @@ export const postJoin = async (req, res) => {
         })
     }
 
-    await User.create({
-        name,
-        email,
-        username,
-        password,
-        location
-    });
-    return res.redirect("/login");
+    try {
+        await User.create({
+            name,
+            email,
+            username,
+            password,
+            location
+        });
+        return res.redirect("/login");
+    } catch (error) {
+        return res.status(400).render("join", {
+            pageTitle: "Upload Video",
+            errorMessage: error._message,
+        });
+    }
 };
 
-export const login = (req, res) => res.send("Login");
+export const getLogin = (req, res) => res.render('login', {pageTitle: 'Login'});
+
+export const postLogin = async (req, res) => {
+    const {username, password} = req.body;
+    const pageTitle = 'Login';
+    const user = await User.findOne({username});
+    if (!user) {
+        return res.status(400).render("login", {
+            pageTitle,
+            errorMessage: "An account with this username does not exists.",
+        });
+    }
+
+    const ok = await bcrypt.compare(password, user.password);
+    if (!ok) {
+        return res.status(400).render("login", {
+            pageTitle,
+            errorMessage: "Wrong password",
+        });
+    }
+
+    return res.redirect("/");
+};
 
 export const logout = (req, res) => res.send("Logout");
 
