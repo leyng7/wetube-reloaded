e4bd6eb2ec14fb9660f4da6097435b6130957b47
unryeong <leyng7@gmail.com>
2021-05-20 20:43:30 +0900

    6.14 Expiration and Secrets

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://192.168.0.15:27017/wetube", {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
