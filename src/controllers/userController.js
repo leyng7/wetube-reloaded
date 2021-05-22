e498a4342f5209860cb0a4e6c8b2533dac666a41
unryeong <leyng7@gmail.com>
2021-05-23 00:11:14 +0900

    7.7 File Uploads part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,15 +8,17 @@ export const postEdit = async (req, res) => {
     const pageTitle = "Edit Profile";
     const {
         session: {
-            user: {_id}
+            user: {_id, avatarUrl}
         },
-        body: {name, email, username, location}
+        body: {name, email, username, location},
+        file
     } = req;
 
     try {
         req.session.user = await User.findByIdAndUpdate(
             _id,
             {
+                avatarUrl: file ? file.path : avatarUrl,
                 name,
                 email,
                 username,
